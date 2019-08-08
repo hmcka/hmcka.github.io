5c38cbe6a500686bd8a9767de78d37cb2567d058
Your Name <you@example.com>
2019-08-07 20:52:45 -0400

    day 6 and 7

---
+++ 100days/day7/countdown.js
@@ -0,0 +1,33 @@
+function countdown() {
+    var now = Date.now();
+    // console.log(now);
+    
+    var Q4A = "School starts in: ";
+    var Sdate = document.getElementById('datebtn').value;
+    var Sday = +new Date(Sdate);
+
+    var remTime = now - Sday;
+
+    var s = Math.floor(remTime / 1000);
+    var m = Math.floor(s / 60);
+    var h = Math.floor(m / 60);
+    var d = Math.floor(h / 24);
+
+    h %= 24;
+    m %= 60;
+    s %= 60;
+
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    document.getElementById("days").textContent = d;
+    document.getElementById("days").innerText = d;
+
+    document.getElementById("hours").textContent = h;
+    document.getElementById("minutes").textContent = m;
+    document.getElementById("seconds").textContent = s;
+     
+    setTimeout(countdown, 1000);
+}
+
