15013d31d77c1a2e5d2fe2a4e7671416ae3d3235
Your Name <you@example.com>
2019-11-17 12:07:12 -0500

    new icons now same size as originals

--- 100days/day61/assets/js/day61-to-do.js
+++ 100days/day61/assets/js/day61-to-do.js
@@ -14,8 +14,7 @@ $("ul").on("click", "span", function(event){
 //Add new to-dos
 $("#pictoBox i").click(function(event){
     var pictoSel = $(this);
-    console.log(pictoSel);
-    $("ul").append('<li><span><i class="fa fa-trash"></i></span><i class="' + pictoSel.attr('class') + '"></i></li>');
+    $("ul").append('<li><span><i class="fa fa-trash fa-lg"></i></span><i class="' + pictoSel.attr('class') + ' fa-lg"></i></li>');
 });
 
 
