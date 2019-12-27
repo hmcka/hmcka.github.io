d22e5cc948a936be198f52ab405758661c08e89f
Your Name <you@example.com>
2019-12-27 02:50:45 -0500

    updated accessibility, DRYness and JS of my personal site

--- js/hamburger-js.js
+++ js/hamburger-js.js
@@ -1,28 +1,17 @@
-$(document).ready(function() {
+var measureMedia = window.matchMedia( "(min-width: 850px)" );
 
-$("#ham-container").click(function(){
-  $(this).toggleClass("active");
-  $("#bar1").toggleClass("active");
-  $("#bar2").toggleClass("active");
-  $("#bar3").toggleClass("active");
-  $("#nav").slideToggle("slow");
-});
-
-var measureMedia = window.matchMedia( "(max-width: 599px)" );
-
-if (measureMedia.matches) {
-  $(".nav-item").click(function(){
-    $(this).toggleClass("active");
-    $("#bar1").toggleClass("active");
-    $("#bar2").toggleClass("active");
-    $("#bar3").toggleClass("active");
-    $("#nav").slideToggle("slow");
-  });
+// Set width of slideNav
+function openNav() {
+    document.getElementById("mySlideNav").style.width = "100%";
 }
-});
 
-
-
-
-
-  
+// Set width back
+function closeNav() {
+    if(measureMedia.matches) {
+        document.getElementById("mySlideNav").style.width = "100%";
+    }
+    else {
+        document.getElementById("mySlideNav").style.width = "0";
+    }
+    
+}
--- js/sliding-nav.js
+++ js/sliding-nav.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
 
-  var measureMedia = window.matchMedia( "(max-width: 599px)" );
+  var measureMedia = window.matchMedia( "(min-width: 850px)" );
 
   if (measureMedia.matches) {
-    var hamHeight = $('ham-container').outerHeight();
+    var hamHeight = $('header').outerHeight();
 
     $('.nav-item').click(function(e) {
     
@@ -16,7 +16,7 @@ $(document).ready(function() {
     })
   }
   else {
-    var headerHeight = $('header').outerHeight();
+    var headerHeight = (($('slideNav').outerHeight())-10);
 
     $('.nav-item').click(function(e) {
     
@@ -30,5 +30,40 @@ $(document).ready(function() {
   }
 
   });
+
+
+
+  // $(document).ready(function() {
+
+  //   var measureMedia = window.matchMedia( "(max-width: 599px)" );
+  
+  //   if (measureMedia.matches) {
+  //     var hamHeight = $('ham-container').outerHeight();
+  
+  //     $('.nav-item').click(function(e) {
+      
+  //       var linkHref = $(this).attr('href');
+  //       $('html, body').animate({
+  //         scrollTop: $(linkHref).offset().top - hamHeight
+  //       }, 800);
+  
+  //       e.preventDefault();
+  //     })
+  //   }
+  //   else {
+  //     var headerHeight = $('header').outerHeight();
+  
+  //     $('.nav-item').click(function(e) {
+      
+  //       var linkHref = $(this).attr('href');
+  //       $('html, body').animate({
+  //         scrollTop: $(linkHref).offset().top - headerHeight
+  //       }, 800);
+  
+  //       e.preventDefault();
+  //     })
+  //   }
+  
+  //   });
 
 
