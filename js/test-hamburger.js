90b903b4fa4e731e7f0e2c1e7fa697b18af4d672
Heather Aluma <hethur@hmcka.com>
2022-05-16 06:49:11 -0400

    Delete test-hamburger.js

--- js/test-hamburger.js
+++
@@ -1,9 +0,0 @@
-// Set width of slideNav
-function openNav() {
-    document.getElementById("mySlideNav").style.width = "100%";
-}
-
-// Set width back
-function closeNav() {
-    document.getElementById("mySlideNav").style.width = "0";
-}
